e6b6f2b8c410e9af2ce85827003b7ae259678e3c
NicolasReyrolle <405928+NicolasReyrolle@users.noreply.github.com>
2017-10-04 21:36:47 +0200

    Testing actual behaviour of the transformation (without input data)

--- src/test/java/org/pentaho/di/trans/kafka/consumer/KafkaConsumerTest.java
+++ src/test/java/org/pentaho/di/trans/kafka/consumer/KafkaConsumerTest.java
@@ -5,6 +5,9 @@ import kafka.consumer.ConsumerConfig;
 import kafka.consumer.ConsumerIterator;
 import kafka.consumer.KafkaStream;
 import kafka.javaapi.consumer.ZookeeperConsumerConnector;
+import kafka.message.Message;
+import kafka.message.MessageAndMetadata;
+import kafka.serializer.DefaultDecoder;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -14,6 +17,9 @@ import org.mockito.Mock;
 import org.pentaho.di.core.KettleEnvironment;
 import org.pentaho.di.core.RowMetaAndData;
 import org.pentaho.di.core.exception.KettleException;
+import org.pentaho.di.core.row.RowMeta;
+import org.pentaho.di.core.row.RowMetaInterface;
+import org.pentaho.di.core.row.value.ValueMetaString;
 import org.pentaho.di.core.variables.Variables;
 import org.pentaho.di.trans.Trans;
 import org.pentaho.di.trans.TransMeta;
@@ -24,10 +30,7 @@ import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -38,6 +41,7 @@ import static org.mockito.Mockito.*;
 public class KafkaConsumerTest {
 
     private static final String STEP_NAME = "Kafka Step";
+    private static final String STEP_LIMIT = "10000";
 
     @Mock
     private Map<String, List<KafkaStream<byte[], byte[]>>> streamsMap;
@@ -61,24 +65,12 @@ public class KafkaConsumerTest {
         KettleEnvironment.init(false);
     }
 
-    @Before
-    public void setUp() {
-        data = new KafkaConsumerData();
-        meta = new KafkaConsumerMeta();
-        meta.setKafkaProperties(getDefaultKafkaProperties());
+    private static Properties getDefaultKafkaProperties() {
+        Properties p = new Properties();
+        p.put("zookeeper.connect", "");
+        p.put("group.id", "");
 
-        stepMeta = new StepMeta("KafkaConsumer", meta);
-        transMeta = new TransMeta();
-        transMeta.addStep(stepMeta);
-        trans = new Trans(transMeta);
-
-        PowerMockito.mockStatic(Consumer.class);
-
-        when(Consumer.createJavaConsumerConnector(any(ConsumerConfig.class))).thenReturn(zookeeperConsumerConnector);
-        when(zookeeperConsumerConnector.createMessageStreams(anyMapOf(String.class, Integer.class))).thenReturn(streamsMap);
-        when(streamsMap.get(anyString())).thenReturn(stream);
-        when(stream.get(anyInt())).thenReturn(kafkaStream);
-        when(kafkaStream.iterator()).thenReturn(streamIterator);
+        return p;
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -128,11 +120,75 @@ public class KafkaConsumerTest {
         assertEquals(0, result.size());
     }
 
-    private Properties getDefaultKafkaProperties() {
-        Properties p = new Properties();
-        p.put("zookeeper.connect", "");
-        p.put("group.id", "");
+    /**
+     * @param rowCount  The number of rows that should be returned
+     * @param hasFields Whether a "UUID" field should be added to each row
+     * @return A RowMetaAndData object that can be used for input data in a test transformation
+     */
+    private static List<RowMetaAndData> generateInputData(int rowCount, boolean hasFields) {
+        List<RowMetaAndData> retval = new ArrayList<RowMetaAndData>();
+        RowMetaInterface rowMeta = new RowMeta();
+        if (hasFields) {
+            rowMeta.addValueMeta(new ValueMetaString("UUID"));
+        }
 
-        return p;
+        for (int i = 0; i < rowCount; i++) {
+            Object[] data = new Object[0];
+            if (hasFields) {
+                data = new Object[]{UUID.randomUUID().toString()};
+            }
+            retval.add(new RowMetaAndData(rowMeta, data));
+        }
+        return retval;
+    }
+
+    private static MessageAndMetadata<byte[], byte[]> generateKafkaMessage() {
+        byte[] message = "aMessage".getBytes();
+
+        return new MessageAndMetadata<byte[], byte[]>("topic", 0, new Message(message),
+                0, new DefaultDecoder(null), new DefaultDecoder(null));
+    }
+
+    @Before
+    public void setUp() {
+        data = new KafkaConsumerData();
+        meta = new KafkaConsumerMeta();
+        meta.setKafkaProperties(getDefaultKafkaProperties());
+        meta.setLimit(STEP_LIMIT);
+
+        stepMeta = new StepMeta("KafkaConsumer", meta);
+        transMeta = new TransMeta();
+        transMeta.addStep(stepMeta);
+        trans = new Trans(transMeta);
+
+        PowerMockito.mockStatic(Consumer.class);
+
+        when(Consumer.createJavaConsumerConnector(any(ConsumerConfig.class))).thenReturn(zookeeperConsumerConnector);
+        when(zookeeperConsumerConnector.createMessageStreams(anyMapOf(String.class, Integer.class))).thenReturn(streamsMap);
+        when(streamsMap.get(anyString())).thenReturn(stream);
+        when(stream.get(anyInt())).thenReturn(kafkaStream);
+        when(kafkaStream.iterator()).thenReturn(streamIterator);
+        when(streamIterator.next()).thenReturn(generateKafkaMessage());
+    }
+
+    // If the step receives rows without any fields, there should be a two output fields (key + value) on each row
+    @Test
+    public void testInputNoFields() throws KettleException {
+        meta.setKeyField("aKeyField");
+        meta.setField("aField");
+
+        when(streamIterator.hasNext()).thenReturn(true);
+
+        TransMeta tm = TransTestFactory.generateTestTransformation(new Variables(), meta, STEP_NAME);
+
+        List<RowMetaAndData> result = TransTestFactory.executeTestTransformation(tm, TransTestFactory.INJECTOR_STEPNAME,
+                STEP_NAME, TransTestFactory.DUMMY_STEPNAME, generateInputData(2, false));
+
+        assertNotNull(result);
+        assertEquals(Integer.parseInt(STEP_LIMIT), result.size());
+        for (int i = 0; i < Integer.parseInt(STEP_LIMIT); i++) {
+            assertEquals(2, result.get(i).size());
+            assertEquals("aMessage", result.get(i).getString(0, "default value"));
+        }
     }
 }
